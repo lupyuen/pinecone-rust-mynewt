cae1a80ef4718fd06cd4b00d9e50090585ace1fe
lupyuen <luppy@appkaki.com>
2020-12-17 03:24:23 +0800

    Add breakpoints

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -4,6 +4,7 @@
 set -e  #  Exit when any command fails
 set -x  #  Echo commands
 
+#  Terminate any OpenOCD processes from the debug session
 set +e  #  Ignore errors
 pkill openocd
 set -e  #  Stop on errors
@@ -15,3 +16,6 @@ set -x  #  Echo commands
 
 #  Build the Mynewt Firmware
 newt build pinecone_app
+
+#  Display the firmware size
+newt size -v pinecone_app
