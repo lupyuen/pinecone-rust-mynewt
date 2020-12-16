9f77650ec49abbee664b72f92db30d0e60919fd0
lupyuen <luppy@appkaki.com>
2020-12-16 16:37:14 +0800

    Fixing rv32imac

--- hw/mcu/bl/bl602/include/env/freedom-e300-hifive1/platform.h
+++ hw/mcu/bl/bl602/include/env/freedom-e300-hifive1/platform.h
@@ -1,4 +1,3 @@
 //  TODO: Remove FE310 hardcoding. This file is referenced by repos/apache-mynewt-core/kernel/os/src/arch/rv32imac/ctx.s
-//  Based on https://github.com/lupyuen/bl_iot_sdk/blob/jtag/components/bl602/bl602/evb/src/boot/gcc/start.S
 
-#define CLINT_CTRL_ADDR 0x02000000
+#include "../platform.h"
---
+++ hw/mcu/bl/bl602/include/env/platform.h
@@ -0,0 +1,8 @@
+//  Based on https://github.com/lupyuen/bl_iot_sdk/blob/jtag/components/bl602/bl602/evb/src/boot/gcc/start.S
+
+#ifndef _BL602_PLATFORM_H
+#define _BL602_PLATFORM_H
+
+#define CLINT_CTRL_ADDR 0x02000000
+
+#endif  //  _BL602_PLATFORM_H
