114ef601599d3a6e90515f04d26fc3ea8d407383
lupyuen <luppy@appkaki.com>
2020-12-23 00:30:20 +0800

    Fixing build

--- hw/mcu/bl/bl602/include/FreeRTOS.h
+++ hw/mcu/bl/bl602/include/FreeRTOS.h
@@ -1,1 +1,9 @@
 //  TODO: Fix the reference to FreeRTOS.h from hw/mcu/bl/bl602/ext/bl_iot_sdk/components/hal_drv/bl602_hal/bl_cks.c
+
+#ifndef __BL602_FREERTOS_H
+#define __BL602_FREERTOS_H
+
+//  Placeholder declarations. Should never be called.
+void vTaskDelay(int xTicksToDelay); 
+
+#endif  //  __BL602_FREERTOS_H
---
+++ hw/mcu/bl/bl602/include/panic.h
@@ -0,0 +1,8 @@
+//  TODO: Fix the reference to panic.h
+#ifndef _PANIC_H_
+#define _PANIC_H_
+
+int backtrace_now(int (*print_func)(const char *fmt, ...), uintptr_t *regs);
+int backtrace_now_app(int (*print_func)(const char *fmt, ...));
+
+#endif
