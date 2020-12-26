ddcc06d710e6f0247c72f0672249348291d699d5
lupyuen <luppy@appkaki.com>
2020-12-26 10:32:12 +0800

    BL602 HAL builds OK yay!

--- hw/mcu/bl/bl602/include/event_groups.h
+++ hw/mcu/bl/bl602/include/event_groups.h
@@ -3,4 +3,16 @@
 #ifndef __BL602_EVENT_GROUPS_H
 #define __BL602_EVENT_GROUPS_H
 
+#define portMAX_DELAY 0
+#define pdFAIL 0
+
+#define EventGroupHandle_t void *
+#define EventBits_t int
+
+int xEventGroupClearBits();
+int xEventGroupWaitBits();
+EventGroupHandle_t xEventGroupCreate();
+void vEventGroupDelete();
+int xEventGroupSetBitsFromISR();
+
 #endif  //  __BL602_EVENT_GROUPS_H
