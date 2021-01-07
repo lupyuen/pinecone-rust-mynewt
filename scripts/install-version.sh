f0de23bbebd3aa4b69926983725b5cee51070577
lupyuen <luppy@appkaki.com>
2021-01-07 23:24:13 +0800

    Add workflow

---
+++ scripts/install-version.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#  Versions of Mynewt and NimBLE to install on all platforms
+
+#  Versions to install
+mynewt_version=mynewt_1_8_0_tag
+nimble_version=nimble_1_3_0_tag
+mcumgr_version=mcumgr_0_1_0_tag
+mcuboot_version=v1.5.0
