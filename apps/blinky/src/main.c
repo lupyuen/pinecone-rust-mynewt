909d90b9684c3f9aa44e83539fd2e3fe0bef6171
lupyuen <luppy@appkaki.com>
2021-01-08 14:41:00 +0800

    Clean up

--- apps/blinky/src/main.c
+++ apps/blinky/src/main.c
@@ -17,81 +17,35 @@
  * under the License.
  */
 
-#include <assert.h>
-#include <string.h>
+#include <sysinit/sysinit.h>  //  Init Functions
+#include <os/os.h>            //  Mynewt Functions
+#include <bsp/bsp.h>          //  Board Support Package
+#include <hal/hal_gpio.h>     //  Mynewt HAL for GPIO
 
-#include "sysinit/sysinit.h"
-#include "os/os.h"
-#include "bsp/bsp.h"
-#include "hal/hal_gpio.h"
-#ifdef ARCH_sim
-#include "mcu/mcu_sim.h"
-#endif
+//  Define the LED GPIOs: 11 (Blue), 14 (Green), 17 (Red)
+#define LED_BLUE_PIN  11
+#define LED_GREEN_PIN 14
+#define LED_RED_PIN   17
 
-int bl_gpio_enable_output(uint8_t pin, uint8_t pullup, uint8_t pulldown);
-int bl_gpio_output_set(uint8_t pin, uint8_t value);
-
-static volatile int g_task1_loops;
-
-/* For LED toggling */
-int g_led_pin;
-
-/**
- * main
- *
- * The main task for the project. This function initializes packages,
- * and then blinks the BSP LED in a loop.
- *
- * @return int NOTE: this function should never return!
- */
-int
-main(int argc, char **argv)
-{
-    int rc;
-
-#ifdef ARCH_sim
-    mcu_sim_parse_args(argc, argv);
-#endif
-
+int main(int argc, char **argv) {
+    //  Initialise Mynewt drivers
     sysinit();
 
-    //  Set GPIO 11 (Blue), 14 (Green), 17 (Red) to output (no pullup, no pulldown)
-    //  Switch off the 3 LEDs (1 = Off)
-    #define LED_BLUE_PIN  11
-    #define LED_GREEN_PIN 14
-    #define LED_RED_PIN   17
+    //  Set GPIO 11 (Blue), 14 (Green), 17 (Red) to output.
+    //  Switch off the 3 LEDs (1 = Off).
     hal_gpio_init_out(LED_BLUE_PIN,  1);
     hal_gpio_init_out(LED_GREEN_PIN, 1);
     hal_gpio_init_out(LED_RED_PIN,   1);
 
-    //  Switch on Blue LED
-    //  hal_gpio_init_out(LED_BLUE_PIN,  0);
+    //  Switch on Blue LED (0 = On)
+    hal_gpio_write(LED_BLUE_PIN, 0);
 
-    //  Switch on Green LED
-    hal_gpio_init_out(LED_GREEN_PIN, 0);
+    //  Switch on Green LED (0 = On)
+    //  hal_gpio_write(LED_GREEN_PIN, 0);
 
-    //  Switch on Red LED
-    //  hal_gpio_init_out(LED_RED_PIN,   0);
+    //  Switch on Red LED (0 = On)
+    //  hal_gpio_write(LED_RED_PIN, 0);
 
     //  Loop forever
     for(;;) {}
-
-#ifdef NOTUSED
-    g_led_pin = LED_BLINK_PIN;
-    hal_gpio_init_out(g_led_pin, 1);
-
-    while (1) {
-        ++g_task1_loops;
-
-        /* Wait one second */
-        os_time_delay(OS_TICKS_PER_SEC);
-
-        /* Toggle the LED */
-        hal_gpio_toggle(g_led_pin);
-    }
-    assert(0);
-#endif  //  NOTUSED
-
-    return rc;
 }
-
